f95bcb3635d5c1d2124dccf73d5fcc6cd10f0664
BearFulmer <bearfulmer@gmail.com>
2026-03-24 19:40:42 -0700

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // the game picks a word at random from a list of words 
+            //The game's state is displayed to the player as shown above 
+            //the player can pick a letter if they pick a letter they already chose pick again 
+            //The game should update its state based on the letter the player picked 
+            //The game needs to detect a win for the player All letters have been guessed 
+            //The game needs to detect a loss for the player out of incorrect guesses 
+
+
+            Console.Title = "Hangman";
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
